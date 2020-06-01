bb62152e63414f520b5113ec56f6d1f8d93f7378
jamesclark111 <cla14033@byui.edu>
2020-06-01 18:00:02 -0400

    First Commit

---
+++ src/build/precache-manifest.1dfde4086364aff4820533b3be1d4229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59441d3b4992cd2c1e60479df3e4ce3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "812cec4b9bfaa3650cad",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8148389af6e6c395e1e8",
+    "url": "/static/js/2.559f4170.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.559f4170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812cec4b9bfaa3650cad",
+    "url": "/static/js/main.0268790d.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
